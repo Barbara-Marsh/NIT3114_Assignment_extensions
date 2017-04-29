180b699b25d1b75b0baa6f134d5fd0d84c28236f
BARB-PC\Barbara Marsh <barbara.marsh@live.vu.edu.au>
2017-04-30 06:11:57 +1000

    Upon login, redirect to user profile page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('layouts/home');
+        //return view('layouts/home');
+
+        return redirect()->route('user.index');
     }
 }
