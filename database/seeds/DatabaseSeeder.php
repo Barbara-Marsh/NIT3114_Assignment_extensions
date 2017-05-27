c58478d4ba644a773c946a71c5cc9e9bfaab5298
Barbara-Marsh <barbara.marsh@live.vu.edu.au>
2017-05-28 01:22:15 +1000

    Added migration and seeds for cities table

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UsersTableSeeder::class);
         $this->call(PlanTableSeeder::class);
+        $this->call(CitiesTableSeeder::class);
         //$this->call(InvoiceTableSeeder::class);
         //$this->call(SubscriptionTableSeeder::class);
     }
