104bbf14a7aa88dfd6e29c2bb2ced26876a0285b
Barbara-Marsh <barbara.marsh@live.vu.edu.au>
2017-05-28 01:33:42 +1000

    Created City class and CityController

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UsersTableSeeder::class);
         $this->call(PlanTableSeeder::class);
         $this->call(CitiesTableSeeder::class);
-        //$this->call(InvoiceTableSeeder::class);
         //$this->call(SubscriptionTableSeeder::class);
     }
 }
