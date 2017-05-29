f8e22d98da2f8f03eb6d442187e5f4c40e0923d5
Barbara-Marsh <barbara.marsh@live.vu.edu.au>
2017-05-30 00:08:03 +1000

    View invoices button now only displays when user is subscribed to a plan

--- resources/views/layouts/user-right-sidebar.blade.php
+++ resources/views/layouts/user-right-sidebar.blade.php
@@ -1,4 +1,7 @@
 @section('user-right-sidebar')
-    <h2>Your invoices</h2>
-    <a href="{{ route('user.invoices') }}" class="btn btn-default btn-margin-top">View Your Invoices</a>
+    @if(isset(Auth::user()->subscription->name))
+        <h2>Your invoices</h2>
+        <a href="{{ route('user.invoices') }}" class="btn btn-default btn-margin-top" id="invoice-btn">View Your Invoices</a>
+    @endif
+
 @show
