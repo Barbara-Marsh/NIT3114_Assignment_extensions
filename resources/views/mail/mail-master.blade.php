a3d70043f44c4f4d5287cb59c692dd970bb28bce
Barbara-Marsh <barbara.marsh@live.vu.edu.au>
2017-06-02 20:52:52 +1000

    Started setting up email blade files so they can be styled

---
+++ resources/views/mail/mail-master.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html lang="{{ config('app.locale') }}">
+<head>
+
+</head>
+<body>
+@yield('content-header')
+
+@yield('content')
+<br>
+<p>Regards,</p>
+<p>The Australian Weather Services team</p>
+<p>aws@example.com</p>
+</body>
+</html>
