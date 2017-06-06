56722295d4c3a2c84a1d0e0ab7f86762eef40fad
Barbara-Marsh <barbara.marsh@live.vu.edu.au>
2017-06-06 17:25:28 +1000

    Admin user redirects to admin dashboard after login

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -27,6 +28,12 @@ class HomeController extends Controller
 
         //return redirect()->route('user.index');
 
-        return redirect()->route('weather.index');
+        //return redirect()->route('weather.index');
+
+        if (Auth::user()->isAdmin()) {
+            return redirect()->route('admin.index');
+        } else {
+            return redirect()->route('weather.index');
+        }
     }
 }
